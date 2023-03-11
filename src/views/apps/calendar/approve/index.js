00c56aa550c25409936223016460429065c176c6
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-03-11 14:21:28 +0545

    admin approve error

--- src/views/apps/calendar/approve/index.js
+++ src/views/apps/calendar/approve/index.js
@@ -3,7 +3,7 @@ import DataTable from 'react-data-table-component'
 import ReactPaginate from 'react-paginate'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'reactstrap'
-import { loadEvent } from '../../../../redux1/action/auth'
+import { loadEvent, updateAdminApproval } from '../../../../redux1/action/auth'
 import { CheckSquare } from 'react-feather'
 const index = () => {
     const dispatch = useDispatch()
@@ -11,18 +11,7 @@ const index = () => {
         dispatch(loadEvent())
     }, [dispatch])
     const data = useSelector(state => state?.Event?.event)
-    console.log(data)
-   
-    const [iconStatus, setIconStatus] = useState([])
-      
-      const isAdminApprovals = useSelector(state => state?.Event?.event?.map(event => ({
-        status: event.isAdminApproval
-       })))
-
-      
-      // setIconStatus(isAdminApprovals)
-     console.log(isAdminApprovals)
-   
+    // console.log(data)
       
     const columns = [
         {
@@ -57,7 +46,9 @@ const index = () => {
         {
             name:'ADMIN APPROVAL',
             minWidth:'100px',
-            selector:  row => (<CheckSquare size={18}  style={{ color: iconStatus ? 'green' : 'red' }}   className='cursor-pointer'  />)
+            selector: row => (<CheckSquare size={18} onClick={() => {
+dispatch(updateAdminApproval(row.uuid, row.isAdminApproval))
+            }} style={{ color: row.isAdminApproval ? 'green' : 'red' }} className='cursor-pointer' />)
         }
     ]
     const paginationComponentOptions = {
