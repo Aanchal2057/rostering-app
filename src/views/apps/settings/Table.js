fcf9628520ef6398e0c83e519e993ecffb3d7c3f
Aanchal2057 <aanchalneupane@gmail.com>
2023-03-14 11:44:50 +0545

    admin status

--- src/views/apps/settings/Table.js
+++ src/views/apps/settings/Table.js
@@ -2,53 +2,68 @@ import DataTable from 'react-data-table-component'
 import { Card } from 'reactstrap'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadEmploee } from '../../../redux1/action/auth'
+import { loadEmploee, updateAdminStatus } from '../../../redux1/action/auth'
 import { CheckSquare } from 'react-feather'
-const columns = [
-    {
-        name: 'Name',
-        selector: row => row.name
-    },
-    {
-        name: 'Email',
-        selector: row => row.email
-    },
-    {
-        name:'Status',
-        selector: row => (<CheckSquare size={18}  style={{ color: row.active ? 'green' : 'red' }}   onClick={() => handleChange(row)}
-        className='cursor-pointer'  />)
-    }
-  
-]
-const paginationComponentOptions = {
-    selectAllRowsItem: true,
-    selectAllRowsItemText: "ALL"
-  }
-
 const Table = () => {
     const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(loadEmploee())
     }, [dispatch])
-    const refetchEvents = () => {
-        dispatch(loadEmploee())
-        
-      }
-    const data = useSelector(state => state?.Employee?.employee)
-    console.log(data)
+
+    const data = useSelector(state => state.Employee.employee)
+
+    const handleChange = ({ uuid, active }) => {
+        dispatch(updateAdminStatus(uuid, { active: !active }))
+    }
+
+    const columns = [
+        {
+            name: 'Name',
+            selector: 'name'
+        },
+        {
+            name: 'Email',
+            selector: 'email'
+        },
+        {
+            name: 'Status',
+            selector: ({ active, uuid }) => (
+                <CheckSquare
+                    size={18}
+                    style={{ color: active ? 'green' : 'red' }}
+                    onClick={() => handleChange({ uuid, active })}
+                    className='cursor-pointer'
+                />
+            )
+        }
+    ]
+
+    const paginationComponentOptions = {
+        selectAllRowsItem: true,
+        selectAllRowsItemText: 'ALL'
+    }
 
     return (
-       <Card style = {{ marginLeft:'-130px' }}>
-         <DataTable
-               
-                   title='Employee List'
-                   pagination
-                    columns={columns}
-                    data={data}
-                    refetchEvents={refetchEvents}
-                    paginationComponentOptions={paginationComponentOptions}
-                />
-       </Card>
+        <Card style={{ marginLeft: '-130px' }}>
+          {Array.isArray(data) && data.length > 0 ? (
+            <DataTable
+                title='Employee List'
+                pagination
+                columns={columns}
+                data={data}
+                refetchEvents={() => dispatch(loadEmploee())}
+                responsive
+                selectableRowsHighlight
+                paginationComponentOptions={paginationComponentOptions}
+                className='react-dataTable'
+            />
+               ) : (
+            <div>No data available</div>
+            
+        )}
+        </Card>
     )
 }
+
 export default Table
